5281116befa5a31e4e40e6355273c1213e7319de
anuragrajanp <anuragrajanp@gmail.com>
2023-09-07 14:10:15 +0530

    Raffle.test.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,41 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "key";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
 
 module.exports = {
+	defaultNetwork: "hardhat",
+	networks: {
+		hardhat: {
+			chainId: 31337,
+			// gasPrice: 130000000000,
+		},
+		sepolia: {
+			url: SEPOLIA_RPC_URL,
+			accounts: [PRIVATE_KEY],
+			chainId: 11155111,
+			blockConfirmations: 6,
+		},
+	},
+	gasReporter: {
+		enabled: false,
+		currency: "USD",
+		outputProfile: "gas-report.txt",
+		noColors: true,
+	},
 	solidity: "0.8.7",
+	namedAccounts: {
+		deployer: {
+			default: 0,
+		},
+		player: {
+			default: 1,
+		},
+	},
+	mocha: {
+		timeout: 30000, // 300 seconds max
+	},
 };
