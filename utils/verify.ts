5cdfdc61b3c3cf9cb26d29d94e2bfbd82ebbc089
anuragrajanp <anuragrajanp@gmail.com>
2023-09-09 15:52:19 +0530

    final

--- utils/verify.ts
+++ utils/verify.ts
@@ -3,7 +3,7 @@ import { run } from "hardhat";
 const verify = async (contractAddress: string, args: any[]) => {
 	console.log("Verifying contract...");
 	try {
-		await run("verify:verfiy", {
+		await run("verify:verify", {
 			address: contractAddress,
 			constructorArguments: args,
 		});
